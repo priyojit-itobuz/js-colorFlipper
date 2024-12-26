e7d4baad9db86a6bca3057115785234481953c2a
Priyojit Kundu <priyojit@itobuz.com>
2024-12-26 18:03:07 +0530

    js-colorFlipper comitted

---
+++ main.js
@@ -0,0 +1,15 @@
+function aqua() {
+    document.body.style.backgroundColor = "aqua";
+}
+
+function blue() {
+    document.body.style.backgroundColor = "blue";
+}
+
+function grey() {
+    document.body.style.backgroundColor = "gray";
+}
+
+function green() {
+    document.body.style.backgroundColor = "green";
+}
